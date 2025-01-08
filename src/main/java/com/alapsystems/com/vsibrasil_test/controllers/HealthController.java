a35c97cc3da6116e75e915fc7dc764ece651a397
André Luiz Alves Pereira <andre.alves@konatussolutions.com>
2025-01-08 14:44:59 -0300

    intial-commit-code-2

---
+++ src/main/java/com/alapsystems/com/vsibrasil_test/controllers/HealthController.java
@@ -0,0 +1,27 @@
+package com.alapsystems.com.vsibrasil_test.controllers;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("health")
+public class HealthController {
+
+    @GetMapping("/")
+    public ResponseEntity<Object> healthCheck(){
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("status", "OK");
+        map.put("message", "API working");
+        log.info("/health -> ".concat(map!=null ? map.toString() : "null"));
+        return new ResponseEntity<Object>(map, HttpStatus.OK);
+    }
+}
